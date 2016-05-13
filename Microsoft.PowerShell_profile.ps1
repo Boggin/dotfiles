79389bda1e8b462712b59db887f63450c805a92d
Richard Bogle <richard.bogle@multrees.com>
2016-05-13 09:49:11 +0100

    Update the install and import of Pscx and VS vars

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
-Import-Module "C:\Program Files (x86)\PowerShell Community Extensions\Pscx3\Pscx"
-Invoke-BatchFile 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat'
+Import-Module Pscx
+Import-VisualStudioVars 140 -Architecture x86
+$Pscx:Preferences.TextEditor = 'notepad2.exe'
 if ($host.Name -eq 'ConsoleHost')
 {
 	Import-Module PSReadline
