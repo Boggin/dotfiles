76c0ee7ca3e65cf2b8fa9a08f2e01472ba85ff4c
Richard Bogle <Boggin@users.noreply.github.com>
2017-05-12 11:41:18 +0100

    Added access to 32-bit module folder

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
-Import-Module Pscx
+$env:PSModulePath = "$env:PSModulePath;C:\Windows\system32\WindowsPowerShell\v1.0\Modules\;"
 Invoke-BatchFile "${env:VS140COMNTOOLS}\vsvars32.bat"
+Import-Module Pscx
 $Pscx:Preferences.TextEditor = "${env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
 if ($host.Name -eq 'ConsoleHost')
 {
@@ -12,3 +13,9 @@ Import-Module -Name posh-git -ErrorAction SilentlyContinue
 Import-Module oh-my-posh
 Import-Module posh-docker
 Set-Theme Sorin
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
