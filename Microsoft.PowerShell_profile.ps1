675d6c9db8ff2ba69fd6fb51ce5e76a176041411
Richard Bogle <richard.bogle@gmail.com>
2017-10-11 15:43:52 +0100

    PowerShell z and hub modules

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,19 @@
 $env:PSModulePath = "$env:PSModulePath;C:\Windows\system32\WindowsPowerShell\v1.0\Modules\;"
-#Invoke-BatchFile "${env:VS140COMNTOOLS}\vsvars32.bat"
-Import-Module Pscx
+# Invoke-BatchFile "${env:VS140COMNTOOLS}\vsvars32.bat"
+# Import-Module Pscx
 $Pscx:Preferences.TextEditor = "${env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
 if ($host.Name -eq 'ConsoleHost')
 {
 	Import-Module PSReadline
 }
 Set-Alias -Name ls -Value PowerLS -Option AllScope
+Set-Alias -Name git -Value hub -Option AllScope
 #try { $null = gcm pshazz -ea stop; pshazz init } catch { }
 # Import oh-my-posh and dependecies
 Import-Module -Name posh-git -ErrorAction SilentlyContinue
 Import-Module oh-my-posh
 Import-Module posh-docker
+Import-Module z
 Set-Theme Sorin
 
 # Chocolatey profile
@@ -19,3 +21,5 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+Set-Alias git hub
